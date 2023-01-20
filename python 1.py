41e71da8e4bf4975bb87a315df854c66557eb13c
MAINA ERIC N <123147966+mainaeric3688@users.noreply.github.com>
2023-01-20 10:05:44 +0300

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,24 @@
+"""
+this is my first 
+"""
+print('hello world')
+y=2.8
+print(type(y))
+list1=[1,2,3,4,5,6,7,8,9]
+print(list1)
+a=100
+b=200
+c=500
+print(a)
+print(b)
+print(c)
+x=500
+y=250
+c=x*y
+print(c)
+x="python is damn "
+y="awesome.Im loving this😊..."
+z=x+y
+print(z)
+import sys
+print(sys.version)
